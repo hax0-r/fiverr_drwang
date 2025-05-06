0dfed9f0cfb6bcfdb4ce183affc37ca159c9984c
Muhammad Talha <talha18513@gmail.com>
2025-05-06 08:46:19 +0500

    changes complete

--- Docs/javascript/Footer.js
+++ Docs/javascript/Footer.js
@@ -26,19 +26,19 @@ footer.innerHTML = `
             <div class="flex max-w-7xl w-full md:items-start items-center mx-auto justify-between md:flex-row flex-col flex-wrap w-full gap-10">
 
                 <div class="">
-                    <img src="./Docs/Assets/home/map.jpg" class="md:w-96 w-full" alt="">
+                    <img src="./Docs/Assets/home/map.jpg" class="md:w-96 w-full imgAppear" alt="">
                 </div>
 
                 <div class="flex flex-col items-center justify-center gap-2 ">
                     <a href="/index.html" class="">
-                        <img src="/Docs/Assets/main-logo.png" class="h-10" alt="">
+                        <img src="/Docs/Assets/main-logo.png" class="h-10 imgAppear" alt="">
                     </a>
-                    <p class="text-gray-700 my-2">15532 92A Ave, Surrey B.C</p>
+                    <p class="text-gray-700 my-2 default">15532 92A Ave, Surrey B.C</p>
                     <a href="tel:+6408323568"
-                        class="text-[#59312c] block bg-white rounded-full py-1 px-3 border w-48 text-center">TEL.
+                        class="text-[#59312c] default block bg-white rounded-full py-1 px-3 border w-48 text-center">TEL.
                         640 832 3568</a>
-                    <p class="text-gray-700 my-2">tcmclinic12@gmail.com</p>
-                                        <p class="text-gray-700 text-center">Clinic hours: <br>
+                    <p class="text-gray-700 default my-2">tcmclinic12@gmail.com</p>
+                                        <p class="text-gray-700 text-center default">Clinic hours: <br>
                         7 days/week 
                         10:30am-6pm </p>
                 </div>
@@ -50,7 +50,7 @@ footer.innerHTML = `
                             target="_blank"
                             class="flex items-center space-x-3 hover:opacity-70 transition-all duration-500">
                             <!-- Google Logo -->
-                            <img src="./Docs/Assets/home/Google__G__logo.svg.png" alt="Google logo" class="w-6 h-6">
+                            <img src="./Docs/Assets/home/Google__G__logo.svg.png" alt="Google logo" class="w-6 imgAppear h-6">
 
                             <!-- Review Info -->
                             <div class="text-sm text-gray-800 mt-2">
@@ -67,7 +67,7 @@ footer.innerHTML = `
                         </a>
                     </div>
                     <a href="/index.html" class="">
-                        <img src="/Docs/Assets/home/award-2017-orange.png" class="md:h-52 w-full" alt="">
+                        <img src="/Docs/Assets/home/award-2017-orange.png" class="md:h-52 imgAppear w-full" alt="">
                     </a>
 
 
@@ -77,8 +77,8 @@ footer.innerHTML = `
             </div>
     </footer>
     <div
-        class="w-full mx-auto text-white bg-[#59312c] p-5 flex items-center justify-center gap-4 text-center flex-col text-gray-700 ">
-        <p class="text-sm">Copyright © Dr.Chun-Kai Jason Wang Surrey Acupuncture and Chinese Medicine, 2017-2024. All
+        class="w-full mx-auto text-white  bg-[#59312c] p-5 flex items-center justify-center gap-4 text-center flex-col text-gray-700 ">
+        <p class="text-sm">Copyright  © Dr.Chun-Kai Jason Wang Surrey Acupuncture and Chinese Medicine, 2017-2024. All
             Rights Reserved. </p>
     </div>
 `
