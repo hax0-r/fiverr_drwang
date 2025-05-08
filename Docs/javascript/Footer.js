15fdd5b987ea04181cf8171f8dadb7bf660e7745
Muhammad Talha <talha18513@gmail.com>
2025-05-08 06:41:12 +0500

    login sign up

--- Docs/javascript/Footer.js
+++ Docs/javascript/Footer.js
@@ -1,6 +1,6 @@
 const footer = document.getElementById('footer')
 footer.innerHTML = `
- <footer class="bg-gray-50 md:p-10 p-5 py-10 md:py-16 ">
+    <footer class="bg-gray-50 md:p-10 p-5 py-10 md:py-16 ">
 
         <div class="fle x hidden items-center justify-center flex-col gap-5">
             <ul class="space-y-2 flex gap-x-10 gap-y-4 justify-center items-center flex-wrap">
@@ -23,58 +23,57 @@ footer.innerHTML = `
             </ul>
         </div>
 
-            <div class="flex max-w-7xl w-full md:items-start items-center mx-auto justify-between md:flex-row flex-col flex-wrap w-full gap-10">
+        <div
+            class=" max-w-7xl w-full md:items-start items-center mx-auto grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
 
-                <div class="">
-                    <img src="./Docs/Assets/home/map.jpg" data-aos="fade" class="md:w-96 w-full imgAppear" alt="">
-                </div>
-
-                <div class="flex flex-col items-center justify-center gap-2 ">
-                    <a href="/index.html" class="">
-                        <img src="/Docs/Assets/main-logo.png" class="h-10 imgAppear" alt="">
-                    </a>
-                    <p class="text-gray-700 my-2 default">15532 92A Ave, Surrey B.C</p>
-                    <a href="tel:+6408323568"
-                        class="text-[#59312c] default block bg-white rounded-full py-1 px-3 border w-48 text-center">TEL.
-                        640 832 3568</a>
-                    <p class="text-gray-700 default my-2">tcmclinic12@gmail.com</p>
-                                        <p class="text-gray-700 text-center default">Clinic hours: <br>
-                        7 days/week 
-                        10:30am-6pm </p>
-                </div>
-
-                <div class="flex flex-col items-center justify-center gap-2 ">
-
-                    <div data-aos="zoom-in" class="flex mb-5 items-center justify-center">
-                        <a href="https://www.google.com/search?q=acupuncture+surrey&oq=acup&gs_lcrp=EgZjaHJvbWUqDAgBECMYJxiABBiKBTIGCAAQRRg5MgwIARAjGCcYgAQYigUyDAgCECMYJxiABBiKBTIGCAMQRRg9MgYIBBBFGD0yBggFEEUYPDISCAYQABhDGLEDGMkDGIAEGIoFMgoIBxAAGJIDGIAEMgoICBAAGLEDGIAEMhAICRAuGK8BGMcBGLoCGIAEMgcIChAAGIAEMgcICxAAGIAEMgoIDBAAGLEDGIAEMgoIDRAAGLEDGIAEMgcIDhAAGIAE0gEHODU4ajBqOagCDrACAfEFz-cs9E1geSzxBc_nLPRNYHks&client=ms-android-samsung-ss&sourceid=chrome-mobile&ie=UTF-8#vhid=/g/11b62t81cz&vssid=lcl"
-                            target="_blank"
-                            class="flex items-center space-x-3 hover:opacity-70 transition-all duration-500">
-                            <!-- Google Logo -->
-                            <img src="./Docs/Assets/home/Google__G__logo.svg.png" alt="Google logo" class="w-6 imgAppear h-6">
-
-                            <!-- Review Info -->
-                            <div class="text-sm text-gray-800 mt-2">
-                                <div class="flex items-center space-x-1">
-                                    <!-- Star Icons -->
-                                    <div class="flex text-yellow-400">
-                                        ★★★★★
-                                    </div>
-                                    <span class="font-semibold text-gray-900">4.8</span>
-                                    <span class="text-gray-500 text-xs">(320 reviews)</span>
-                                </div>
-                                <span class="underline text-blue-600">Read our Google Reviews</span>
-                            </div>
-                        </a>
-                    </div>
-                    <a href="/index.html" class="">
-                        <img src="/Docs/Assets/home/award-2017-orange.png" data-aos="fade" class="md:h-52 imgAppear w-full" alt="">
-                    </a>
-
-
-                </div>
-
-
+            <div class="">
+                <img src="./Docs/Assets/home/map.jpg" data-aos="fade" class="w-full " alt="">
             </div>
+
+            <div class="flex flex-col items-center justify-center gap-2 ">
+                <a href="/index.html" class="">
+                    <img src="/Docs/Assets/main-logo.png" class="h-10 imgAppear" alt="">
+                </a>
+                <p class="text-gray-700 my-2 default">15532 92A Ave, Surrey B.C</p>
+                <a href="tel:+6048323568"
+                    class="text-[#59312c] default block bg-white rounded-full py-1 px-3 border w-48 text-center">TEL.
+                    604 832 3568</a>
+                <p class="text-gray-700 default my-2">tcmclinic12@gmail.com</p>
+                <p class="text-gray-700 text-center default">Clinic hours: <br>
+                    7 days/week
+                    10:30am-6pm </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center gap-2 ">
+
+                <div data-aos="zoom-in" class="flex mb-5 items-center justify-center">
+                    <a href="https://g.co/kgs/BKFu5jV "
+                        target="_blank"
+                        class="flex items-center space-x-3 hover:opacity-70 transition-all duration-500">
+                        <!-- Google Logo -->
+                        <img src="./Docs/Assets/home/Google__G__logo.svg.png" alt="Google logo"
+                            class="w-6 imgAppear h-6">
+
+                        <!-- Review Info -->
+                        <div class="text-sm text-gray-800 mt-2">
+                            <div class="flex items-center space-x-1">
+                                <!-- Star Icons -->
+                                <div class="flex text-yellow-400">
+                                    ★★★★★
+                                </div>
+                                <span class="font-semibold text-gray-900">4.8</span>
+                                <span class="text-gray-500 text-xs">(39 reviews)</span>
+                            </div>
+                            <span class="underline text-blue-600">Read our Google Reviews</span>
+                        </div>
+                    </a>
+                </div>
+                <a href="/index.html" class="">
+                    <img src="/Docs/Assets/home/award-2017-orange.png" data-aos="fade" class="md:h-52 w-full"
+                        alt="">
+                </a>
+            </div>
+        </div>
     </footer>
     <div
         class="w-full mx-auto text-white  bg-[#59312c] p-5 flex items-center justify-center gap-4 text-center flex-col text-gray-700 ">
